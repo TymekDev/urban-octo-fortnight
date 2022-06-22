f4af4e33f0b5e5f8b01602389a955270fe5345db
Tymoteusz Makowski <t@makowski.sh>
2022-06-22 09:26:03 +0200

    feat: add constructors

--- model.go
+++ model.go
@@ -4,12 +4,30 @@ type Model map[User]UserData
 
 type User string
 
+func NewUser(username string) User {
+	return User(username)
+}
+
 type UserData struct {
 	Iron   Factory
 	Copper Factory
 	Gold   Factory
 }
 
+func NewUserData() UserData {
+	return UserData{
+		Iron:   NewFactory(),
+		Copper: NewFactory(),
+		Gold:   NewFactory(),
+	}
+}
+
 type Factory struct {
 	Level int
+}
+
+func NewFactory() Factory {
+	return Factory{
+		Level: 1,
+	}
 }
